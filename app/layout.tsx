4b911c5f05710e97798c5223b896bac5e32f0f3f
Nico Dann <nicodann@gmail.com>
2024-01-22 14:16:15 -0500

    Add new google font.  Edit homepage text.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Noto_Serif } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const noto = Noto_Serif({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={noto.className}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <h1>
+        Nico Dann | Full Stack Web Developer
+      </h1>
     </main>
   )
 }
