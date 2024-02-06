76a0cf7a5938eb9ae83b31536b45021d3c304d4d
Nico Dann <nicodann@gmail.com>
2024-02-06 10:40:49 -0500

    Make letters disappear when clicked

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,41 @@
+"use client";
+
 import links from '@/data/links.json'
 import Link from 'next/link'
+import { Fragment, useEffect, useState } from 'react'
 
 export default function Header() {
+  const name = 'Nico Dann'
+  const letterArray = name.split('')
+  const [nameArray, setNameArray] = useState(letterArray)
+  
+  useEffect(() => {
+    console.log("nameArray:",nameArray)
+  }, [nameArray]);
+
+  
   return (
     <header className="flex flex-col lg:flex-row gap-12 flex-wrap justify-between lg:w-full text-center">
-          <h1>
-            Nico Dann
-          </h1>
+        <div className='flex bg-red-50'>
+          {nameArray.length !== 1
+            ? nameArray.map((letter, i) => (
+              <h2 key={i} onClick={() => {
+                  setNameArray((prev) => {
+                    console.log("prev:",prev)
+                    return prev.toSpliced(i,1)
+                  })
+                }}>
+                  {letter}
+              </h2>
+              ))
+            : <h2 onClick={() => setNameArray(letterArray)}>RESET</h2>
+            }
+{/* 
+         {letterArray.map((letter, i) => <h2 key={i} onClick={() => {
+            letterArray.splice(i,1)
+            console.log("New:", letterArray)
+          }}>{letter}</h2>)} */}
+        </div>
 
           <section className="flex flex-col gap-6 sm:gap-0">
             <h2>
@@ -15,7 +44,8 @@ export default function Header() {
 
             <section className="flex flex-col sm:flex-row gap-2 justify-center lg:justify-end flex-wrap">
               {links.map((link,i) => (
-                <>
+                <Fragment key={i}>
+              
                   <h2 key={i}>
                     <Link href={link.href}>
                       {link.name}
@@ -25,7 +55,7 @@ export default function Header() {
                   {i < links.length -1 && 
                     <h2 className='hidden sm:inline'>|</h2>
                   }
-                </>
+                </Fragment>
               )
               )}
             </section>
