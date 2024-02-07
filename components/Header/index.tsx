44ba85a2d68ed2cdf64313d797cfa5a3fe048873
Nico Dann <nicodann@gmail.com>
2024-02-07 09:59:48 -0500

    Fix letter logic.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -13,10 +13,20 @@ export default function Header({setBgColour}: {setBgColour: (value:string) => vo
     }
   })
 
-  const bgColours = ['#D6DBDC', '#526760','#374B4A','#DA3E52', '#FE5F00', '#000000'];
+  const bgColours = ['#D6DBDC', '#526760','#374B4A','#DA3E52', '#FE5F00'];
   const [colourIndex, setColourIndex] = useState(1);
   const [nameArray, setNameArray] = useState(letterArray)
-  const [isHoveredOver, setIsHoveredOver] = useState(false)
+  const [isHoveredOver, setIsHoveredOver] = useState<{[key:number]:boolean}>({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
+    7: false,
+    8: false
+  })
 
   useEffect(() => {
     console.log("isHoveredOver:",isHoveredOver)
@@ -44,17 +54,25 @@ export default function Header({setBgColour}: {setBgColour: (value:string) => vo
   return (
     <header className="flex flex-col lg:flex-row gap-12 flex-wrap justify-between lg:w-full text-center">
         <div className='flex justify-center cursor-pointer'>
-          {nameArray.length !== 1
-            ? nameArray.map((letter, i) => (
-              <h1 key={i} onMouseOver={() => setIsHoveredOver(true)} onMouseLeave={() => setIsHoveredOver(false)} onClick={() => handleLetterClick(i)} style={{
-                background:isHoveredOver ? bgColours[colourIndex + 2] : ''
-              }}>
-                  {letter}
-              </h1>
-              ))
-            : <h1 className='text-white' onClick={() => handleResetClick()}>
+          {nameArray.length === 0 || nameArray.length === 1 && nameArray[0] === '\u00A0'
+            ? 
+              <h1 className='text-white' onClick={() => handleResetClick()}>
                 RESET
               </h1>
+            :
+              nameArray.map((letter, i) => (
+                <h1 
+                  key={i} 
+                  onMouseOver={() => setIsHoveredOver((prev) => {return {...prev, [i]:true}})} 
+                  onMouseLeave={() => setIsHoveredOver((prev) => {return {...prev, [i]: false}})} 
+                  onClick={() => handleLetterClick(i)} 
+                  style={{
+                    background:isHoveredOver[i] ? bgColours[colourIndex + 2] : ''
+                  }}
+                >
+                    {letter}
+                </h1>
+                ))       
             }
         </div>
 
