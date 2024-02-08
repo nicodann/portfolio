8d7aadaa918096837a28f61b6e7f8f6a806d7868
Nico Dann <nicodann@gmail.com>
2024-02-08 12:11:21 -0500

    Move reset button.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -66,43 +66,75 @@ export default function Header({setBgColour}: {setBgColour: (value:string) => vo
   }
 
   return (
-    <header className="flex flex-col lg:flex-row gap-12 flex-wrap justify-between lg:w-full text-center">
-      <div id="#interactive_heading" className='flex justify-center lg:justify-between'>
-        <div className='flex justify-center cursor-pointer'>
-          {letterArrayUnicode.map((letter, i) => {
-            return (
-              <h1
-                key={i}
-                onClick={() => handleLetterClick(letter)} 
-                style={{position: 'relative',
-                  top: !letterObject[letter] ? '0px' : '-400px',
-                  transition: 'top, 1s'
-                }}
-              >
-                  {letter}
-              </h1>
-            )})        
-          }
+    <header className='flex flex-col items-start'>
+      <div className="flex flex-col lg:flex-row gap-12 flex-wrap justify-between lg:w-full text-center">
+        <div id="#interactive_heading" className='flex justify-center lg:justify-between'>
+          <div className='flex justify-center cursor-pointer'>
+            {letterArrayUnicode.map((letter, i) => {
+              return (
+                <h1
+                  key={i}
+                  onClick={() => handleLetterClick(letter)} 
+                  style={{position: 'relative',
+                    top: !letterObject[letter] ? '0px' : '-400px',
+                    transition: 'top, 1s'
+                  }}
+                >
+                    {letter}
+                </h1>
+              )})        
+            }
+          </div>
         </div>
+
+        <div id='#second_heading_plus_nav' className='flex gap-6 flex-wrap'>
+          <div id='#second_name' className='flex text-black'>
+            {letterArrayUnicode.map((letter, i) => {
+              return (
+                <h1
+                  key={i}
+                  style={{position: 'relative',
+                    top: letterObject[letter] ? '0px' : '-400px',
+                    transition: 'top, 1s'
+                  }}
+                >
+                    {letter}
+                </h1>
+              )})     
+            }
+          </div>
+
+          <div id='#subtitle_nav' className="flex flex-col gap-6 sm:gap-0">
+            <h2 style={{color:bgColours[colourIndex + 1]}}>
+              Full Stack Web Developer
+            </h2>
+
+            <nav className="flex flex-col sm:flex-row gap-2 justify-center lg:justify-end flex-wrap">
+              {links.map((link,i) => (
+                <Fragment key={i}>
+              
+                  <h2 key={i}>
+                    <Link href={link.href}>
+                      {link.name}
+                    </Link>
+                  </h2>
+
+                  {i < links.length -1 && 
+                    <h2 className='hidden sm:inline'>|</h2>
+                  }
+                </Fragment>
+              )
+              )}
+            </nav>
+
+          </div>
+
+
+
+        </div>
+
       </div>
-
-      <div id='#second_heading_plus_nav' className='flex gap-6 flex-wrap'>
-        <div id='#second_name' className='flex text-black'>
-          {letterArrayUnicode.map((letter, i) => {
-            return (
-              <h1
-                key={i}
-                style={{position: 'relative',
-                  top: letterObject[letter] ? '0px' : '-400px',
-                  transition: 'top, 1s'
-                }}
-              >
-                  {letter}
-              </h1>
-            )})     
-          }
-        </div>
-
+      <button>
         <h1 
           className='text-white cursor-pointer' 
           onClick={() => handleResetClick()}
@@ -112,33 +144,7 @@ export default function Header({setBgColour}: {setBgColour: (value:string) => vo
         >
           RESET
         </h1>
-
-        <div id='#subtitle_nav' className="flex flex-col gap-6 sm:gap-0">
-          <h2 style={{color:bgColours[colourIndex + 1]}}>
-            Full Stack Web Developer
-          </h2>
-
-          <nav className="flex flex-col sm:flex-row gap-2 justify-center lg:justify-end flex-wrap">
-            {links.map((link,i) => (
-              <Fragment key={i}>
-            
-                <h2 key={i}>
-                  <Link href={link.href}>
-                    {link.name}
-                  </Link>
-                </h2>
-
-                {i < links.length -1 && 
-                  <h2 className='hidden sm:inline'>|</h2>
-                }
-              </Fragment>
-            )
-            )}
-          </nav>
-
-        </div>
-      </div>
-
+      </button>
     </header>
   )
 }
