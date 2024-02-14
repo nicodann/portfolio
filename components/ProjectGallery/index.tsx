5180e962017ee06af4a84e17acf875d6faa9e9df
Nico Dann <nicodann@gmail.com>
2024-02-14 10:52:19 -0500

    Style project boxes and fix hyperlink logic.

--- components/ProjectGallery/index.tsx
+++ components/ProjectGallery/index.tsx
@@ -16,44 +16,45 @@ export default function ProjectGallery() {
 
   const filteredProjects = projects.filter(project => project.image_url)
 
-  const ProjectBox = ({i, project}:{i: number, project: Project}) => (
-    <div key={i} className="flex flex-col gap-6 bg-amber-50 p-4 justify-between w-72 sm:w-80">
-      <div className="flex flex-col gap-4">
-        <h2>{project.name}</h2>
-        <div className='relative min-h-32'>
-          <Image src={project.image_url} alt={project.name} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-top" priority/>
-        </div>
-        <p id="project_description" className="max-w-80">{project.description}</p>
-      </div>
-      <footer className='flex justify-between'>
-        <Link href={project.github_url}>github</Link>
-        {project.url && <Link href={project.url}>{project.name}</Link>}
-      </footer>
-    </div>
+  
+  
+  const ProjectBox = ({i, project}:{i: number, project: Project}) => {
+    const links: string[] = [
+      project.github_url,
+    ]
 
-  )
+    project.url && links.push(project.url)
+    
+    return (
+      <div key={i} className="flex flex-col gap-6 bg-amber-50 p-4 justify-between w-72 sm:w-80 rounded-2xl shadow-2xl">
+        <Link href={project.url ? project.url : project.github_url}>
+          <div className="flex flex-col gap-4 text-black">
+            <h2>{project.name}</h2>
+            <div className='relative min-h-32'>
+              <Image src={project.image_url} alt={project.name} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-top" priority/>
+            </div>
+            <p id="project_description" className="max-w-80">{project.description}</p>
+          </div>
+        </Link>
+        <footer className='flex justify-between'>
+          {
+            links.map((link, i) => (
+              <Link 
+                key={i} 
+                href={link}
+              >
+                {i === 0 ? 'github' : project.name}
+              </Link>
+            ))
+          }            
+        </footer>
+      </div>  
+    )
+  }
 
   return (
     <section className="flex lg:flex-row justify-center flex-wrap gap-4">
-        {filteredProjects.map((project, i) => ProjectBox({project, i})
-        // (
-
-          // <div key={i} className="flex flex-col gap-6 bg-amber-50 p-4 justify-between w-72 sm:w-80">
-          //   <div className="flex flex-col gap-4">
-          //     <h2>{project.name}</h2>
-          //     <div className='relative min-h-32'>
-          //       <Image src={project.image_url} alt={project.name} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover object-top" priority/>
-          //     </div>
-          //     <p id="project_description" className="max-w-80">{project.description}</p>
-          //   </div>
-          //   <footer className='flex justify-between'>
-          //     <Link href={project.github_url}>github</Link>
-          //     {project.url && <Link href={project.url}>{project.name}</Link>}
-          //   </footer>
-          // </div>
-
-        // )
-        )}
+        {filteredProjects.map((project, i) => ProjectBox({ project, i }))}
       </section>
   )
 }
