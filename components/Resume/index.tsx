d933f55021d9d8f7c82af02fa758f03b3ee1460d
Nico Dann <nicodann@gmail.com>
2025-02-25 12:25:15 -0500

    Add resume to main page and fix header link.

--- components/Resume/index.tsx
+++ components/Resume/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useWindowWidth } from '@/hooks/useWindowWidth';
+import { useEffect, useState } from 'react';
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
 
 export default function Resume() {
+  const windowWidth: number = useWindowWidth()
+  const [pdfWidth, setPdfWidth] = useState<number>(0);
+
+  
+  useEffect(() => {
+    if (windowWidth < 500) {
+      setPdfWidth(300);
+    } else if (windowWidth > 500 && windowWidth < 768) {
+      setPdfWidth(500);
+    }  else if (windowWidth > 768 && windowWidth < 1024) {
+      setPdfWidth(768);
+    } else if (windowWidth > 1024) {
+      setPdfWidth(1024);
+    } 
+  }, [windowWidth]);
+
   return (
-    <iframe src="/files/Nico_Dann-Web_Developer_Resume_2025.pdf"/>
+    <div id="resume" className="flex justify-center">
+      <Document
+        file="/files/Nico_Dann-Web_Developer_Resume_2025.pdf"
+        loading="Loading..."
+      >
+        <Page 
+          pageNumber={1} 
+          renderTextLayer={false} 
+          renderAnnotationLayer={false}
+          width={pdfWidth}
+        />
+      </Document>
+    </div>
   )
 }
---
+++ hooks/useWindowWidth.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function useWindowWidth() {
+	const [windowWidth, setWindowWidth] = useState(0);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		handleResize();
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+	return windowWidth;
+}
